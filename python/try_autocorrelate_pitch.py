c23622eea725ff63d48516bfa79346cb3b0c2044
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2023-11-27 19:33:32 -0500

    Smoothing in both time and the frequency domain

--- python/try_autocorrelate_pitch.py
+++ python/try_autocorrelate_pitch.py
@@ -22,6 +22,49 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Frequency Spectrum')
 
 
+class FrequencyHistory:
+    def __init__(self, num_frequencies, fast_smoothing_factor=0.7, slow_smoothing_factor=0.95, decay_rate=0.01):
+        """
+        num_frequencies: Number of frequency bins.
+        fast_smoothing_factor: Smoothing factor for the fast average (closer to 1 for slower decay).
+        slow_smoothing_factor: Smoothing factor for the slow average.
+        decay_rate: Decay rate for the slow average.
+        """
+        self.fast_history = np.zeros(num_frequencies)
+        self.slow_history = np.zeros(num_frequencies)
+        self.fast_smoothing_factor = fast_smoothing_factor
+        self.slow_smoothing_factor = slow_smoothing_factor
+        self.decay_rate = decay_rate
+
+    def update(self, current_frame):
+        """
+        Update the history with the current frame data.
+        Applies different exponential smoothing to fast and slow histories.
+        """
+        self.fast_history = self.fast_smoothing_factor * self.fast_history + (
+                    1 - self.fast_smoothing_factor) * current_frame
+        self.slow_history = self.slow_smoothing_factor * self.slow_history * (1 - self.decay_rate) + (
+                    1 - self.slow_smoothing_factor) * current_frame
+
+    def get_smoothed_frequency(self, smoothing_width=3):
+        """
+        Return the frequency data after subtracting the slow smoothed rolling average from the fast one,
+        and then applying frequency-domain smoothing.
+        smoothing_width: Number of neighboring frequencies to include in the smoothing.
+        """
+        # Subtract the slow average from the fast average
+        diff_average = self.fast_history - self.slow_history
+
+        # Apply frequency-domain smoothing
+        smoothed = np.convolve(diff_average, np.ones(smoothing_width) / smoothing_width, mode='same')
+        return smoothed
+
+
+# TODO a better way to get this number
+frequency_history = FrequencyHistory(num_frequencies=662, decay_rate=0.01)
+
+
+
 def find_fundamental_frequency(audio_data, sample_rate):
     # Perform autocorrelation
     corr = np.correlate(audio_data, audio_data, mode='full')
@@ -92,16 +135,22 @@ try:
         # Apply A-weighting to the FFT magnitudes
         a_weighted_fft = fft_magnitude * a_weighting_curve(fft_freq)
 
+        # Smoothing
+        frequency_history.update(a_weighted_fft)
+        smoothed_frequency = frequency_history.get_smoothed_frequency()
+
+        display_freqs = smoothed_frequency
+
         # Clear screen
         screen.fill((0, 0, 0))
 
         # Draw bars for each frequency
-        bar_width = WIDTH // len(a_weighted_fft)
-        for i, freq in enumerate(a_weighted_fft):
+        bar_width = WIDTH // len(display_freqs)
+        for i, freq in enumerate(display_freqs):
             x = i * bar_width
             y = HEIGHT
             # height = fft_magnitude[i] / 100 * HEIGHT # Scale for display
-            magnitude_db = convert_to_decibels(fft_magnitude[i])
+            magnitude_db = convert_to_decibels(display_freqs[i])
             height = np.interp(magnitude_db, [0, 60], [0, HEIGHT])  # Scale dB to screen height
             draw_bar(screen, x, HEIGHT - height, bar_width + 1, height, (0, 255, 0))
 
